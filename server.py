c322464cc2e540d850ae27e57aecc668a81c58a6
javierpb275 <javierpb275@gmail.com>
2021-11-30 18:58:15 +0100

    route submit_form: showing data in console and redirect to thankyou page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -15,4 +15,9 @@ def html_page(page_name):
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return 'form submitted!'
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('./thankyou.html')
+    else:
+        return 'Something went wrong. Try again!'
