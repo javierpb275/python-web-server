917250f4ef4692b25d6ce5e93f2cbfc8da423a61
javierpb275 <javierpb275@gmail.com>
2021-11-30 17:38:23 +0100

    formatted files

--- server.py
+++ server.py
@@ -2,21 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/index.html")
 def my_home():
     return render_template('./index.html')
+
 
 @app.route("/works.html")
 def works():
     return render_template('./works.html')
 
+
 @app.route("/about.html")
 def about():
     return render_template('./about.html')
 
+
 @app.route("/contact.html")
 def contact():
     return render_template('./contact.html')
+
 
 @app.route("/components.html")
 def components():
