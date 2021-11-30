48b49a3376fd808378d912dc725cfcf968d4d184
javierpb275 <javierpb275@gmail.com>
2021-11-30 19:35:26 +0100

    created write to csv function that saves the data in a csv file by using the csv package
    also added try except to the submit form function

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+
 def write_to_file(data):
     with open('database.txt', mode='a') as database:
         email = data['email']
@@ -20,11 +22,24 @@ def write_to_file(data):
         file = database.write(f'\n{email},{subject},{message}')
 
 
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
-        return redirect('./thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('./thankyou.html')
+        except:
+            return 'did no t save to database'
     else:
         return 'Something went wrong. Try again!'
