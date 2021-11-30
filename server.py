b31759f81c44f48ec5d64ab79ca001a6081084df
javierpb275 <javierpb275@gmail.com>
2021-11-30 17:15:54 +0100

    added templates and created routes for each template

--- server.py
+++ server.py
@@ -2,18 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    return render_template('./index.html', name=username, post_id=post_id)
+@app.route("/index.html")
+def my_home():
+    return render_template('./index.html')
+
+@app.route("/works.html")
+def works():
+    return render_template('./works.html')
 
 @app.route("/about.html")
 def about():
     return render_template('./about.html')
 
-@app.route("/blog")
-def blog():
-    return "<p>My blog</p>"
+@app.route("/contact.html")
+def contact():
+    return render_template('./contact.html')
 
-@app.route("/blog/2021/dogs")
-def blog2():
-    return "<p>My dog</p>"
+@app.route("/components.html")
+def components():
+    return render_template('./components.html')
