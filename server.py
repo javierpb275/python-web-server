0cb424a58afdc5d4d72674803fa7cf542ef2af77
javierpb275 <javierpb275@gmail.com>
2021-11-30 18:11:14 +0100

    created dynamic route that accepts the name of the page as a parameter.
    also changed some text in the html files.

--- server.py
+++ server.py
@@ -3,26 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/index.html")
+@app.route("/")
 def my_home():
     return render_template('./index.html')
 
 
-@app.route("/works.html")
-def works():
-    return render_template('./works.html')
-
-
-@app.route("/about.html")
-def about():
-    return render_template('./about.html')
-
-
-@app.route("/contact.html")
-def contact():
-    return render_template('./contact.html')
-
-
-@app.route("/components.html")
-def components():
-    return render_template('./components.html')
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
