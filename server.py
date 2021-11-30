97e9acaec63171aca5251bbb614dded2e0f2ba83
javierpb275 <javierpb275@gmail.com>
2021-11-30 16:16:52 +0100

    passing parameters to url and display those parameters in the template

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('./index.html')
+@app.route("/<username>/<int:post_id>")
+def hello_world(username=None, post_id=None):
+    return render_template('./index.html', name=username, post_id=post_id)
 
 @app.route("/about.html")
 def about():
