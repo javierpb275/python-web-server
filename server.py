c18941387c306dfe5ccae152ef2cc0cf45b84327
javierpb275 <javierpb275@gmail.com>
2021-11-30 15:44:21 +0100

    created routes and templates folder and moved html there. also rendering html using render_template

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('./index.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('./about.html')
+
+@app.route("/blog")
+def blog():
+    return "<p>My blog</p>"
+
+@app.route("/blog/2021/dogs")
+def blog2():
+    return "<p>My dog</p>"
